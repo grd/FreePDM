60b603a4806a10272c6772b5ceb584bb1748a3a9
grd <skotje@posteo.nl>
2022-06-02 13:21:50 +0200

    still very bad but now it's working

--- src/Skeleton/directorymodel.py
+++ src/Skeleton/directorymodel.py
@@ -1,21 +1,29 @@
+"""
+    :copyright: Copyright 2022 by the FreePDM team
+    :license:   MIT License.
+"""
+
 import os
+from itemdatamodel import ItemDataModel
 
 # The directorymodel is a list of files that can be used inside a GUI
-
-from itemdatamodel import ItemDataModel
 
 class DirectoryModel():
 	def __init__(self, dir):
 		self.dir = dir
 		self.dirList = {}
+		self.getDirList()
 
-	def getList(self):
+	def getDirList(self):
 		dir_list = os.listdir(self.dir)
 		for x in dir_list:
 			# eleminate files and directories starting with "."
 			if x.startswith("."):
 				continue
-			if x.contains(".FCStd"):
+			if ".FCStd" in x:
 				self.dirList[x] = ItemDataModel(x) 
+			else:
+				self.dirList[x] = x
+
 
 
--- src/Skeleton/main.py
+++ src/Skeleton/main.py
@@ -1,7 +1,15 @@
+"""
+    :copyright: Copyright 2022 by the FreePDM team
+    :license:   MIT License.
+"""
 
-  
+from directorymodel import DirectoryModel
+
+# right now it should only show a directory that you specify
+#  and print the values about that directory 
 def main():
-  
+    dm = DirectoryModel("/home/user/temp")
+    print(dm.dirList)
   
 if __name__=="__main__":
     main();
