c3df7cd7071fb3600e7e19077e40c03baf575fef
Jee-Bee <winkeltj@e.email>
2022-07-21 16:15:00 +0200

    Add main main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from src.GUI.MainWindow import main
+
+# https://pythonspot.com/pyqt5-grid-layout/
+# https://doc.qt.io/qtforpython/overviews/layout.html
+# See structure https://github.com/tlecomte/friture
+
+if __name__ == "__main__":
+    main()
