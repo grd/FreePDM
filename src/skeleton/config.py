0d11df80365081fa30e692917119eed55e2e6de5
grd <skotje@posteo.nl>
2022-07-28 13:19:06 +0200

    Update configuration

--- src/skeleton/config.py
+++ src/skeleton/config.py
@@ -3,11 +3,13 @@
     :license:   MIT License.
 """
 
+import os
 import configparser
+import appdirs
 
 # Reading and writing the configuration file.
-# The location of the file is: (I don't know yet, and the name too).
-# The file format is probably ini since that is the easiest.
+# The location of the file is: [prefix location]/FreePDM/FreePDM.conf
+#   [prefix location] depends on which OS you use...
 #
 #  The items are:
 #    * The start of the project directory (or directories).
@@ -15,15 +17,39 @@ import configparser
 #        "startupdir"
 #
 
-class generalOptions(object):
-    def __init__(self):
-        self.load_config()
+appname = 'FreePDM'
+config_dir = appdirs.user_config_dir(appname)
+config_name = os.path.join(config_dir, 'FreePDM.conf')
 
-    def load_config(self):
-        config = configparser.ConfigParser()
-        config['DEFAULT'] = {'StartupDirectory': '',
-                            'Filter': '',
-                            'LogFile': '',
-                            'LogLevel': ''}
-        with open('example.ini', 'w') as configfile:
-            config.write(configfile)
+###
+### Variables
+###
+
+startup_directory = ''
+filter = ''
+log_file = ''
+log_level = ''
+fast_loading_dir =  ''
+
+
+def read():
+    config = configparser.ConfigParser()
+    config.read(config_name)
+
+    # reading variables from section: 'DEFAULT'
+    startup_directory = config['DEFAULT']['startup_directory']
+    filter =            config['DEFAULT']['filter']
+    log_file =          config['DEFAULT']['log_file']
+    log_level =         config['DEFAULT']['log_levle']
+    fast_loading_dir =  config['DEFAULT']['fast_loading_dir']
+
+def write():
+    config = configparser.ConfigParser()
+    config['DEFAULT']['startup_directory'] = startup_directory
+    config['DEFAULT']['filter'] = filter
+    config['DEFAULT']['log_file'] = log_file
+    config['DEFAULT']['log_levle'] = log_level
+    config['DEFAULT']['fast_loading_dir'] = fast_loading_dir
+
+    with open(config_name, 'w') as configfile:
+        config.write(configfile)
