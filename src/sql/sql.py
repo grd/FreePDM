6133318a30a4801474f801df9c4b46f2ec5ebe20
Jee-Bee <winkeltj@e.email>
2022-08-01 19:27:43 +0200

    A lot of small changes

--- src/sql/sql.py
+++ src/sql/sql.py
@@ -6,7 +6,8 @@
 """
 
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Boolean, Integer, Float, String, Date, ForeignKey
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Boolean, Integer, Float, String, Date, Enum, LargeBinary
 from typing import NewType, Optional
 
 Base = declarative_base()
@@ -32,12 +33,14 @@ class SQLUser(Base):
 
     user_id = Column(Integer, primary_key=True)
     user_name = Column(String(30))
-    first_name = Column(String(30))
-    last_name = Column(String(30))
-    full_name = Column(String)
-    email_adress = Column(String, nullable=False)
-    # phonenumber =   # TODO: add phonenumber property
-    # role = []  # TODO: add List of roles
+    user_first_name = Column(String(30))
+    user_last_name = Column(String(30))
+    user_full_name = Column(String)
+    user_email_adress = Column(String, nullable=False)  # TODO: change to mail address
+    user_phonenumber = Column(Integer)  # TODO: add phonenumber property
+    # user_role = []  # TODO: add List of roles
+    user_department = Column(String)  # Enum optionally
+    # user_aliases = []  # TODO: What to do with aliases
 
 
 class SQLRole(Base):
@@ -56,6 +59,9 @@ class SQLProject(Base):
     project_id = Column(Integer, primary_key=True)
     project_number = Column(Integer)  # this can come another source as the Db so not same as project_id
     project_name = Column(String(32))
+    project_status = Column(Enum)  # TODO: Create Enum list
+    Project_date_start = Column(Date)
+    Project_date_finish = Column(Date)  # TODO check finsh date is after start date
     project_path = Column(String)
 
 
@@ -68,9 +74,13 @@ class SQLItem(Base):
     item_name = Column(String(32))
     item_description = Column(String(32))
     item_full_description = Column(String)
+    item_number_linked_files = Column(Integer)
     # item should be able to exist in multiple projects. but need a singel store location
     item_in_project = Column(Integer, ForeignKey('projects.project_number'), nullable=False)
     item_path = Column(String)
+    # TODO: get image from Model. If there is no fileimage add default empty image.
+    item_preview = Column(LargeBinary)  # Change when no image is available
+    purchasing_id = Column(Integer, ForeignKey('purchasing.purchasing_id'))
 
 
 class SQLModel(Base):
@@ -82,10 +92,11 @@ class SQLModel(Base):
     model_name = Column(String(32))
     model_description = Column(String(32))
     model_full_description = Column(String)
-    model_filename = Column(String(255))  # Limit of current file systems
+    model_filename = Column(String(253), nullable=False)  # Limit of current file systems
     # Auto calculate extension
-    model_ext = Column(String(255))  # Total limit of filename and extension is 255
+    model_ext = Column(String(253), nullable=False)  # Total limit of filename and extension is 255
     # model_path = Column(String)  # should belongs to same path as described in item
+    model_preview = Column(LargeBinary)  # Change when no image is available
 
 
 class SQLDocument(Base):
@@ -97,9 +108,9 @@ class SQLDocument(Base):
     document_name = Column(String(32))
     document_description = Column(String(32))
     document_full_description = Column(String)
-    document_filename = Column(String(255))  # Limit of current file systems
+    document_filename = Column(String(253), nullable=False)  # Limit of current file systems
     # Auto calculate extension
-    document_ext = Column(String(255))  # Total limit of filename and extension is 255
+    document_ext = Column(String(253), nullable=False)  # Total limit of filename and extension is 255
     # document_path = Column(String)  # should belongs to same path as described in item
 
 
@@ -110,10 +121,14 @@ class SQLModelMaterial(Base):
     modelmat_id = Column(Integer, primary_key=True)
     model_material = Column(String(32))
     model_finish = Column(String(32))
-    model_mass = Column(Float)  # unit mass
-    model_volume = Column(Float)
+    model_mass = Column(Float)
+    model_mass_unit = Column(Enum)  # TODO: Create Enum list
+    model_volume = Column(Float)  # TODO: From CAD file
+    model_volume_unit = Column(Enum)  # TODO: Create Enum list
     model_weight = Column(Float)  # model_mass * model_volume
+    model_weight_unit = Column(Enum)  # TODO: Create Enum list
     model_surface_area = Column(Float)
+    model_surface_area_unit = Column(Enum)  # TODO: Create Enum list
     model_number = Column(Integer, ForeignKey('models.model_number'), nullable=False)
 
 
@@ -128,10 +143,11 @@ class SQLHistory(Base):
     history_date_last_edit = Column(Date)
     history_last_edit_by = Column(String)
     history_checked_out_by = Column(String)
-    history_revision_state = Column(String)
+    history_revision_state = Column(Enum)  # TODO: Create Enum list
     history_revision_number = Column(Integer)  # Maybe other format
     # TODO: Create Complex revisions (Example: Date, major.minor, major.letter_minor)
-    history_store_number = Column(Integer)  # last store version iterator
+    history_stored_number = Column(Integer)  # last store version iterator
+    # TODO: Every stored version in the database should be traceble
 
 
 class SQLPurchase(Base):
@@ -140,7 +156,9 @@ class SQLPurchase(Base):
 
     purchasing_id = Column(Integer, primary_key=True)
     purchasing_source = Column(Boolean)  # Repesent Buy OR Manufacture
-    purchasing_tracebility = Column(String)  # Represent list: Lot, Lot And Serial Number, Serial Number, Not traced
+    # Optionally Enum
+    purchasing_tracebility = Column(Enum)  # TODO: Create Enum list
+    # Represent list: Lot, Lot And Serial Number, Serial Number, Not traced
     manufacturer_id = Column(Integer, ForeignKey('manufacturers.manufacturer_id'))
     vendor_id = Column(Integer, ForeignKey('vendors.vendor_id'))
 
