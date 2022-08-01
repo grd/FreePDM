3a42c87e520eebe2b2035d95ec562dd85d5c8eec
Jee-Bee <winkeltj@e.email>
2022-08-01 13:33:44 +0200

    Add initial SQL, User, Role classes

---
+++ src/sql/sql.py
@@ -0,0 +1,47 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2022 by the FreePDM team
+    :license:   MIT License.
+"""
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+from typing import NewType, Optional
+
+Base = declarative_base()
+
+
+class SQL(Base):
+    """Generic SQL"""
+    # https://docs.python.org/3/library/typing.html#typing.NamedTuple
+
+    def __init__(self):
+        # self.Base = declarative_base()
+        print("Generic SQL")
+
+    def connect_db(self):
+        """Connect to database"""
+        # https://docs.sqlalchemy.org/en/14/tutorial/dbapi_transactions.html
+        print("connect to database")
+
+
+class SQLUser(Base):
+    """SQL User"""
+    __tablename__ = 'user_account'
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String(30))
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    full_name = Column(String)
+    email_adress = Column(String, nullable=False)
+    # phonenumber =   # TODO: add phonenumber property
+    # role = []  # TODO: add List of roles
+
+
+class SQLRole(Base):
+    """Class for genrating Roles"""
+    __tablename__ = 'roles'
+    role_id = Column(Integer, primary_key=True)
+    role_name = Column(String(30))
+    # TODO: add privileges - Also how to
