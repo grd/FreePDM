7e02a1f1c0b26e3f2cf957393afede0bf92f5895
Jee-Bee <winkeltj@e.email>
2022-08-02 21:02:06 +0200

    small changes

--- src/sql/createdb.py
+++ src/sql/createdb.py
@@ -9,6 +9,7 @@ from sql import SQLUser, SQLRole, SQLProject, SQLItem, SQLModel, SQLDocument, SQ
 
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from typing import NewType, Optional
 
 Base = declarative_base()
@@ -42,13 +43,16 @@ class CreateMySQLDb(CreateDb):  # Alles in een file of beter te splitsen?
         print("MySQL")
         super(CreateMySQLDb, self).__init__()
 
-    def start_engine(self, encoding: str, echo: bool, future: bool, dialect: Optional[str]):
+    def start_engine(self, url: str, encoding: str, echo: bool, future: bool, dialect: Optional[str]):
         """
         Start MySQL engine.
         Note: MySQL engine is not default developement database. 
 
         Parameters
         ----------
+
+        url [str] : url
+            Path to MySQL database.
 
         encoding [str] : Set text encoding for database.
             The default encoding is `utf-8`
@@ -62,6 +66,7 @@ class CreateMySQLDb(CreateDb):  # Alles in een file of beter te splitsen?
         dialect [string] : If other SQL python libraies are used this can be set.
             Optional parameter. 
         """
+        self.url = url
         self.encoding = encoding
         self.echo = echo
         self.future = future
@@ -70,18 +75,17 @@ class CreateMySQLDb(CreateDb):  # Alles in een file of beter te splitsen?
         if (self.dialect == "default") or (self.dialect is None):
             # Installing via `FreePDM-ServerInstaller.sh` installs default engine
             # default
-            engine = create_engine('mysql://scott:tiger@localhost/foo')
+            engine = create_engine('mysql://scott:tiger@localhost/foo', echo=self.echo, future=self.future)
             pass
         elif (self.dialect == "mysqlclient") or (self.dialect == "mysqldb"):
             # mysqlclient (a maintained fork of MySQL-Python)
-            engine = create_engine('mysql+mysqldb://scott:tiger@localhost/foo')
+            engine = create_engine('mysql+mysqldb://scott:tiger@localhost/foo', echo=self.echo, future=self.future)
             pass
         elif (self.dialect == "PyMySQL") or (self.dialect == "pymysql"):
             # PyMySQL
-            engine = create_engine('mysql+pymysql://scott:tiger@localhost/foo')
+            engine = create_engine('mysql+pymysql://scott:tiger@localhost/foo', echo=self.echo, future=self.future)
         else:
             pass
-        engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)  # start from memory
 
 
 class CreatePostgreSQLDb(CreateDb):
@@ -92,12 +96,15 @@ class CreatePostgreSQLDb(CreateDb):
         print("PostgreSQL")
         super(CreatePostgreSQLDb, self).__init__()
 
-    def start_engine(self, encoding: str, echo: bool, future: bool, dialect: Optional[str]):
+    def start_engine(self, url: str, encoding: str, echo: bool, future: bool, dialect: Optional[str]):
         """
         Start PostgreSQL engine.
 
         Parameters
         ----------
+
+        url [str] : url
+            Path to PostgreSQL database.
 
         encoding [str] : Set text encoding for database.
             The default encoding is `utf-8`
@@ -111,6 +118,7 @@ class CreatePostgreSQLDb(CreateDb):
         dialect [string] : If other SQL python libraies are used this can be set.
             Optional parameter. 
         """
+        self.url = url
         self.encoding = encoding
         self.echo = echo
         self.future = future
@@ -118,15 +126,15 @@ class CreatePostgreSQLDb(CreateDb):
         # https://docs.sqlalchemy.org/en/14/core/engines.html#database-urls
         if (self.dialect == "default") or (self.dialect is None):
             # default
-            engine = create_engine('postgresql://scott:tiger@localhost/mydatabase')
+            engine = create_engine('postgresql://scott:tiger@localhost/mydatabase', echo=self.echo, future=self.future)
             pass
         elif self.dialect == "psycopg2":
             # psycopg2
-            engine = create_engine('postgresql+psycopg2://scott:tiger@localhost/mydatabase')
+            engine = create_engine('postgresql+psycopg2://scott:tiger@localhost/mydatabase', echo=self.echo, future=self.future)
             pass
         elif self.dialect == "pg8000":
             # pg8000
-            engine = create_engine('postgresql+pg8000://scott:tiger@localhost/mydatabase')
+            engine = create_engine('postgresql+pg8000://scott:tiger@localhost/mydatabase', echo=self.echo, future=self.future)
         else:
             pass
 
@@ -139,13 +147,16 @@ class CreateSQLiteDb(CreateDb):  # Alles in een file of beter te splitsen?
         super(CreateSQLiteDb, self).__init__()
         print("SQLite")
 
-    def start_engine(self, encoding: str, echo: bool, future: bool):
+    def start_engine(self, url: str, encoding: str, echo: bool, future: bool):
         """
         Start SQLite engine.
         Note: SQLite engine is not default developement database. 
 
         Parameters
         ----------
+
+        url [str] : url
+            Path to SQLite database.
 
         encoding [str] : Set text encoding for database.
             The default encoding is `utf-8`
@@ -156,14 +167,16 @@ class CreateSQLiteDb(CreateDb):  # Alles in een file of beter te splitsen?
         future [bool] : Future proof style
             SQLAlchemy 2.0 up style Engine, Connection (Introduced in SQLAlchemy 1.4).
         """
+        self.url = url
         self.encoding = encoding
         self.echo = echo
         self.future = future
         # https://docs.sqlalchemy.org/en/14/core/engines.html#database-urls
-        engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)  # start from memory
+        engine = create_engine("sqlite+pysqlite:///:memory:", echo=self.echo, future=self.future)  # start from memory
 
 
 def start_the_engine():
+    """Start your engine"""
     pass
 
 
