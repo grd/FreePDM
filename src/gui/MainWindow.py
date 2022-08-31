2c2401a67265fe9d2764606e74342965b9f54198
grd <skotje@posteo.nl>
2022-08-31 16:24:16 +0200

    Changed layout. Now the MainWindow.ui file is gone

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -8,9 +8,8 @@ from pathlib import Path
 import sys
 
 from PySide2 import QtCore, QtWidgets, QtGui
-from PySide2.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QMessageBox
-from PySide2.QtCore import QFile, Qt
-from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem, QMessageBox
+from PySide2.QtCore import Qt
 
 from .EditItem import *
 from .filter import *
@@ -20,127 +19,144 @@ sys.path.append(os.fspath(Path(__file__).resolve().parents[1] / 'skeleton'))
 from directorymodel import DirectoryModel
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
 
+class Ui_MainWindow(object):
+    def setup_ui(self, MainWindow):
         self.root_directory = os.path.expanduser('~')
         if len(sys.argv) == 2:
             self.root_directory = sys.argv[1]
         print("self.root_directory = ", self.root_directory)
         self.current_directory = self.root_directory
 
-        self.load_ui()
-        self.load_data()
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(817, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.layoutButtonbar = QtWidgets.QHBoxLayout()
+        self.layoutButtonbar.setObjectName("layoutButtonbar")
+        self.buttonCheckIn = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonCheckIn.setObjectName("buttonCheckIn")
+        self.layoutButtonbar.addWidget(self.buttonCheckIn)
+        self.buttonCheckOut = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonCheckOut.setObjectName("buttonCheckOut")
+        self.layoutButtonbar.addWidget(self.buttonCheckOut)
+        self.buttonCheckInOut = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonCheckInOut.setObjectName("buttonCheckInOut")
+        self.layoutButtonbar.addWidget(self.buttonCheckInOut)
+        self.lineCI2P = QtWidgets.QFrame(self.centralwidget)
+        self.lineCI2P.setFrameShape(QtWidgets.QFrame.VLine)
+        self.lineCI2P.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.lineCI2P.setObjectName("lineCI2P")
+        self.layoutButtonbar.addWidget(self.lineCI2P)
+        self.buttonPurge = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonPurge.setObjectName("buttonPurge")
+        self.layoutButtonbar.addWidget(self.buttonPurge)
+        self.buttonFilter = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonFilter.setObjectName("buttonFilter")
+        self.layoutButtonbar.addWidget(self.buttonFilter)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.layoutButtonbar.addItem(spacerItem)
+        self.buttonSearch = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonSearch.setObjectName("buttonSearch")
+        self.layoutButtonbar.addWidget(self.buttonSearch)
+        self.gridLayout.addLayout(self.layoutButtonbar, 0, 0, 1, 1)
+        self.tableWorkspace = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWorkspace.setObjectName("tableWorkspace")
+        self.tableWorkspace.setColumnCount(7)
+        self.tableWorkspace.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWorkspace.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWorkspace.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWorkspace.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWorkspace.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWorkspace.setHorizontalHeaderItem(4, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWorkspace.setHorizontalHeaderItem(5, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWorkspace.setHorizontalHeaderItem(6, item)
+        self.gridLayout.addWidget(self.tableWorkspace, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 817, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-    def load_ui(self):
-        loader = QUiLoader()
-        path = os.fspath(Path(__file__).resolve().parents[1] / "gui/MainWindow.ui")
-        print(path)
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        self.ui = loader.load(ui_file, self)
-        self.ui.setGeometry(50, 40, 800, 600)
-        # Some change below based on https://pythonprogramming.net/basic-gui-pyqt-tutorial/
-        self.ui.setWindowTitle("FreePDM")  # Done in ui file
-        # self.ui.setWindowIcon(QtGui.QIcon(os.fspath(Path(__file__).resolve().parents[1] / "ui/logos/O_logo-32x32.png")))  # Probably done in ui file OSX don't show icon
+        self.tableWorkspace.setColumnWidth(0, 5)
+        self.tableWorkspace.setColumnWidth(1, 200)
+        self.tableWorkspace.setColumnWidth(2, 200)
+        self.tableWorkspace.setColumnWidth(3, 80)
+        self.tableWorkspace.setColumnWidth(4, 80)
+        self.tableWorkspace.setColumnWidth(5, 80)
+        self.tableWorkspace.verticalHeader().setVisible(False)
+        self.tableWorkspace.setSelectionBehavior(QTableWidget.SelectRows)
 
-        self.ui.tableWorkspace.setColumnWidth(0, 10)
-        self.ui.tableWorkspace.setColumnWidth(1, 150)
-        self.ui.tableWorkspace.setColumnWidth(2, 200)
-        self.ui.tableWorkspace.setColumnWidth(3, 80)
-        self.ui.tableWorkspace.setColumnWidth(4, 80)
-        self.ui.tableWorkspace.setColumnWidth(5, 80)
-        self.ui.tableWorkspace.verticalHeader().setVisible(False)
-        self.ui.tableWorkspace.setColumnWidth(0, 30)
-        self.ui.tableWorkspace.setSelectionBehavior(QTableWidget.SelectRows)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.buttonPurge.clicked.connect(self.purge)
+
+        self.tableWorkspace.setSelectionBehavior(QTableWidget.SelectRows)
         # self.ui.tableWorkspace.selectionModel().selectionChanged.connect(self.on_selection_changed)
-        self.ui.tableWorkspace.doubleClicked.connect(self.file_double_clicked)
+        self.tableWorkspace.doubleClicked.connect(self.file_double_clicked)
         # self.ui.buttonCheckOutButton('Check In', clicked=self.retrieve_check_button_values)
-        self.ui.buttonFilter.clicked.connect(self.set_filter)
-        self.ui.buttonPurge.clicked.connect(self.purge)
+        self.buttonFilter.clicked.connect(self.set_filter)
+        self.buttonPurge.clicked.connect(self.purge)
 
-    def set_filter(self):
-        filter_dialog()
         self.load_data()
 
-    # deal with doubleclick
-    def file_double_clicked(self, event):
-        row = event.row()
-        item = self.ui.tableWorkspace.item(row, 5).text()
-
-        # Change directory
-        if item == 'Directory':
-            dir = self.ui.tableWorkspace.item(row, 1).text()
-            self.current_directory = os.path.abspath(os.path.join(self.current_directory, dir))
-            self.load_data()
-
-        # Edit FC Item
-        if item == 'FCStd' or item == 'A2-Assy' or item == 'A3-Assy'or item == 'A4-Assy':
-            part = self.ui.tableWorkspace.item(row, 1).text()
-            part = os.path.abspath(os.path.join(self.current_directory, part))
-            edit_item_dialog(part)
-            self.load_data()
- 
-  
-
-    # deal with checkbox click on a field
-    def retrieve_check_button_values(self):
-        for row in range(self.ui.tableWidget.rowCount()):
-            if self.ui.tableWorkspace.item(row, 0).checkState() == Qt.CheckState.Checked:
-                print("selected row: ", row)
-
-    # deal with the rows
-    def on_selection_changed(self, selected, deselected):
-        for ix in selected.indexes():
-            print('Selected Cell Location Row: {0}, Column: {1}'.format(ix.row(), ix.column()))
-
-        for ix in deselected.indexes():
-            print('Deselected Cell Location Row: {0}, Column: {1}'.format(ix.row(), ix.column()))
 
     def load_data(self):
         self.dirmodel = DirectoryModel(self.current_directory, self.root_directory != self.current_directory)
         row = 0
-        self.ui.tableWorkspace.setRowCount(self.dirmodel.size())
+        self.tableWorkspace.setRowCount(self.dirmodel.size())
         # https://stackoverflow.com/questions/39511181/python-add-checkbox-to-every-row-in-qtablewidget
         for item in self.dirmodel.directoryList:
             checkb = QTableWidgetItem("")
             checkb.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
             checkb.setCheckState(Qt.CheckState.Unchecked)
-            self.ui.tableWorkspace.setItem(row, 0, checkb)
+            self.tableWorkspace.setItem(row, 0, checkb)
 
             file = QTableWidgetItem(item['filename'])
             file.setFlags(file.flags() ^ Qt.ItemIsEditable)
             if item['type'] == 'Directory':
                 file.setForeground(QtGui.QColor('blue'))
-            self.ui.tableWorkspace.setItem(row, 1, file)
+            self.tableWorkspace.setItem(row, 1, file)
 
             description = QTableWidgetItem('')
             description.setFlags(description.flags() ^ Qt.ItemIsEditable)
-            self.ui.tableWorkspace.setItem(row, 2, description)
+            self.tableWorkspace.setItem(row, 2, description)
 
             state = QTableWidgetItem('')
             state.setFlags(state.flags() ^ Qt.ItemIsEditable)
-            self.ui.tableWorkspace.setItem(row, 3, state)
+            self.tableWorkspace.setItem(row, 3, state)
 
             version = QTableWidgetItem('')
             version.setFlags(version.flags() ^ Qt.ItemIsEditable)
-            self.ui.tableWorkspace.setItem(row, 4, version)
+            self.tableWorkspace.setItem(row, 4, version)
 
             itemtype = QTableWidgetItem(item['type'])  # type is default function in python
             itemtype.setFlags(itemtype.flags() ^ Qt.ItemIsEditable)
-            self.ui.tableWorkspace.setItem(row, 5, itemtype)
+            self.tableWorkspace.setItem(row, 5, itemtype)
 
             size = QTableWidgetItem(item['size'])
             size.setTextAlignment(Qt.AlignRight)
             size.setFlags(size.flags() ^ Qt.ItemIsEditable)
-            self.ui.tableWorkspace.setItem(row, 6, size)
+            self.tableWorkspace.setItem(row, 6, size)
             row += 1
 
         if len(self.dirmodel.purge_list) == 0:
-            self.ui.buttonPurge.setEnabled(False)
+            self.buttonPurge.setEnabled(False)
         else:
-            self.ui.buttonPurge.setEnabled(True)
+            self.buttonPurge.setEnabled(True)
 
 
     def purge(self):
@@ -158,14 +174,78 @@ class MainWindow(QMainWindow):
         if i.text() == '&OK':
          self.dirmodel.purge()
 
+    
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.buttonCheckIn.setText(_translate("MainWindow", "Check In"))
+        self.buttonCheckOut.setText(_translate("MainWindow", "Check Out"))
+        self.buttonCheckInOut.setText(_translate("MainWindow", "Check In/Out"))
+        self.buttonPurge.setText(_translate("MainWindow", "Purge"))
+        self.buttonFilter.setText(_translate("MainWindow", "Filter"))
+        self.buttonSearch.setText(_translate("MainWindow", "Search"))
+        item = self.tableWorkspace.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Name"))
+        item = self.tableWorkspace.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Description"))
+        item = self.tableWorkspace.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "State"))
+        item = self.tableWorkspace.horizontalHeaderItem(4)
+        item.setText(_translate("MainWindow", "Version"))
+        item = self.tableWorkspace.horizontalHeaderItem(5)
+        item.setText(_translate("MainWindow", "Type"))
+        item = self.tableWorkspace.horizontalHeaderItem(6)
+        item.setText(_translate("MainWindow", "Size"))
+
+
+    def set_filter(self):
+        filter_dialog()
+        self.load_data()
+
+    # deal with doubleclick
+    def file_double_clicked(self, event):
+        row = event.row()
+        item = self.tableWorkspace.item(row, 5).text()
+
+        # Change directory
+        if item == 'Directory':
+            dir = self.tableWorkspace.item(row, 1).text()
+            self.current_directory = os.path.abspath(os.path.join(self.current_directory, dir))
+            self.load_data()
+
+        # Edit FC Item
+        if item == 'FCStd' or item == 'A2-Assy' or item == 'A3-Assy'or item == 'A4-Assy':
+            part = self.tableWorkspace.item(row, 1).text()
+            part = os.path.abspath(os.path.join(self.current_directory, part))
+            edit_item_dialog(part)
+            self.load_data()
+ 
+  
+
+    # deal with checkbox click on a field
+    def retrieve_check_button_values(self):
+        for row in range(self.tableWidget.rowCount()):
+            if self.tableWorkspace.item(row, 0).checkState() == Qt.CheckState.Checked:
+                print("selected row: ", row)
+
+    # deal with the rows
+    def on_selection_changed(self, selected, deselected):
+        for ix in selected.indexes():
+            print('Selected Cell Location Row: {0}, Column: {1}'.format(ix.row(), ix.column()))
+
+        for ix in deselected.indexes():
+            print('Deselected Cell Location Row: {0}, Column: {1}'.format(ix.row(), ix.column()))
+
+
 
 def main():
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
-    app = QApplication(sys.argv)
-    widget = MainWindow()
-    widget.ui.show()
+    app = QtWidgets.QApplication(sys.argv)
+    ex = Ui_MainWindow()
+    w = QMainWindow()
+    ex.setup_ui(w)
+    w.show()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     mainw = main()
