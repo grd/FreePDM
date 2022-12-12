4210203dc811d595a1825b655e9251f6620b496b
Jee-Bee <winkeltj@e.email>
2022-12-12 18:35:58 +0100

    Reordening classes on alphabetic order

--- src/database/default_tables.py
+++ src/database/default_tables.py
@@ -17,70 +17,55 @@ import default_enum
 # https://www.geeksforgeeks.org/metadata-in-dbms-and-its-types/
 
 
-class PdmUser(Base):
-    """Class with default SQL User properties"""
-    __tablename__ = 'users'
+class PdmDocument(Base):
+    """Class with SQL Item properties"""
+    __tablename__ = 'documents'
 
-    user_id = Column(Integer, primary_key=True)
-    user_name = Column(String(30))
-    user_first_name = Column(String(30))
-    user_last_name = Column(String(30))
-    user_full_name = Column(String)
-    user_email_adress = Column(String, nullable=False)  # TODO: change to mail address
-    user_phonenumber = Column(Integer)  # TODO: add phonenumber property
-    user_department = Column(String)  # Enum optionally
-    # user_aliases = []  # TODO: What to do with aliases
+    document_id = Column(Integer, primary_key=True)
+    document_number = Column(Integer)  # or should this be a string?
+    document_name = Column(String(32))
+    document_description = Column(String(32))
+    document_full_description = Column(String)
+    document_filename = Column(String(253), nullable=False)  # Limit of current file systems
+    # Auto calculate extension
+    document_ext = Column(String(253), nullable=False)  # Total limit of filename and extension is 255
+    # document_path = Column(String)  # should belongs to same path as described in item
 
     # relationships with other tables
-    roles = relationship("PdmRole", secondary="user_role_link", back_populates="users")
-    projects = relationship("PdmProjects", secondary="user_project_link", back_populates="users")
-    items = relationship("PdmItem", back_populates="user")
-    models = relationship("PdmModel", back_populates="user")
-    documents = relationship("PdmDocument", back_populates="user")
+    user_id = Column(Integer, ForeignKey("user_accounts.user_id"))
+    user = relationship("PdmUser", back_populates="documents")
+    item_id = Column(Integer, ForeignKey("items.item_id"))
+    item = relationship("PdmItem", back_populates="models")
 
     def __repr__(self):
         # Fstrings are better ?
         # https://realpython.com/python-f-strings/
-        return f"PdmUser(user_id={self.user_id!r}, user_name={self.user_name!r}, user_first_name={self.user_first_name!r}, user_last_name={self.user_last_name!r}, user_full_name={self.user_full_name!r}, user_email_adress={self.user_email_adress!r}, user_phonenumber={self.user_phonenumber!r}, user_department={self.user_department!r})"
+        return(f"PdmDocument(document_id={self.document_id!r}, document_number={self.document_number!r}, document_name={self.document_name!r}, document_description={self.document_description!r}, document_full_description={self.document_full_description!r}, document_filename={self.document_filename!r}, document_ext={self.document_ext!r})")
 
 
-class PdmRole(Base):
-    """Class with default SQL Role properties"""
-    __tablename__ = 'roles'
+# TODO: A histric item / Model / Document can  have a foreign key to all of those tables.
+class PdmHistory(Base):
+    """Class with SQL History properties"""
+    __tablename__ = 'history'
 
-    role_id = Column(Integer, primary_key=True)
-    role_name = Column(String(32))
-    # TODO: add privileges - Also how to
+    history_id = Column(Integer, primary_key=True)
+    history_date_created = Column(Date)
+    history_created_by = Column(String)
+    history_date_last_edit = Column(Date)
+    history_last_edit_by = Column(String)
+    history_checked_out_by = Column(String)
+    # TODO: Create Complex revisions (Example: Date, major.minor, major.letter_minor)
+    history_revision_state = Column(Enum(pdm_enum.RevisionState))
+    history_revision_number = Column(Integer)  # Maybe other format
+    history_stored_number = Column(Integer)  # last store version iterator
+    # TODO: Every stored version in the database should be traceble
 
     # relationships with other tables
-    user_id = Column(Integer, ForeignKey("users.user_id"))
-    users = relationship("PdmUser", secondary="user_role_link", back_populates="roles")
 
     def __repr__(self):
         # Fstrings are better ?
         # https://realpython.com/python-f-strings/
-        return(f"PdmRole(role_id={self.role_id!r}, role_name={self.role_name!r})")
-
-
-class PdmProject(Base):
-    """Class with default SQL Role properties"""
-    __tablename__ = 'projects'
-
-    project_id = Column(Integer, primary_key=True)
-    project_number = Column(String(16), nullable=False)  # this can come another source as the Db so not same as project_id
-    project_name = Column(String(32))
-    project_status = Column(Enum(pdm_enum.ProjectState))
-    Project_date_start = Column(Date)
-    Project_date_finish = Column(Date)  # TODO check finish date is after start date
-    project_path = Column(String)
-
-    # relationships with other tables
-    users = relationship("PdmUsers", secondary="user_project_link", back_populates="projects")
-
-    def __repr__(self):
-        # Fstrings are better ?
-        # https://realpython.com/python-f-strings/
-        return(f"PdmProject(project_id={self.project_id!r}, project_number={self.project_number!r}, project_name={self.project_name!r}, project_status={self.project_status!r}, project_date_start={self.project_date_start!r}, project_date_finish={self.project_date_finish!r}, project_path={self.project_path!r})")
+        return(f"PdmHistory(history_id={self.history_id!r}, history_date_created={self.history_date_created!r}, history_created_by={self.history_created_by!r}, history_date_last_edit={self.history_date_last_edit!r}, history_last_edit_by={self.history_last_edit_by!r}, history_checked_out_by={self.history_checked_out_by!r}, history_revision_state={self.history_revision_state!r}, history_revision_number={self.history_revision_number!r}, history_stored_number={self.history_stored_number!r})")
 
 
 class PdmItem(Base):
@@ -114,58 +99,22 @@ class PdmItem(Base):
         return(f"PdmItem(item_id={self.item_id!r}, tem_number={self.item_number!r}, item_name={self.item_name!r}, item_description={self.item_description!r}, item_full_description={self.item_full_description!r}, item_number_linked_files={self.item_number_linked_files!r}, item_path={self.item_path!r}, item_preview={self.item_preview!r})")
 
 
-class PdmModel(Base):
-    """Class with SQL Item properties"""
-    __tablename__ = 'models'
+class PdmManufacturer(Base):
+    """Class with SQL Manufacturing properties"""
+    __tablename__ = 'manufacturers'
 
-    model_id = Column(Integer, primary_key=True)
-    model_number = Column(Integer)  # or should this be a string?
-    model_name = Column(String(32))
-    model_description = Column(String(32))
-    model_full_description = Column(String)
-    model_filename = Column(String(253), nullable=False)  # Limit of current file systems
-    # Auto calculate extension
-    model_ext = Column(String(253), nullable=False)  # Total limit of filename and extension is 255
-    # model_path = Column(String)  # should belongs to same path as described in item
-    model_preview = Column(LargeBinary)  # Change when no image is available
+    manufacturer_id = Column(Integer, primary_key=True)
+    manufacturer_name = Column(String(32))
+    # TODO: Add manufacturer address
 
     # relationships with other tables
-    user_id = Column(Integer, ForeignKey("users.user_id"))
-    user = relationship("PdmUser", back_populates="models")
-    item_id = Column(Integer, ForeignKey("items.item_id"))
-    item = relationship("PdmItem", back_populates="models")
-    material = relationship("PdmMaterial", back_populates="model", uselist=False)
+    purchasing_id = Column(Integer, ForeignKey("purchasing.purchasing_id"))
+    purchasing = relationship("PdmPurchase", back_populates="manufacturers")
 
     def __repr__(self):
         # Fstrings are better ?
         # https://realpython.com/python-f-strings/
-        return(f"PdmModel(model_id={self.model_id!r}, model_number={self.model_number!r}, model_name={self.model_name!r}, model_description={self.model_description!r}, model_full_description={self.model_full_description!r}, model_filename={self.model_filename!r}, model_ext={self.model_ext!r}, model_preview={self.model_preview!r})")
-
-
-class PdmDocument(Base):
-    """Class with SQL Item properties"""
-    __tablename__ = 'documents'
-
-    document_id = Column(Integer, primary_key=True)
-    document_number = Column(Integer)  # or should this be a string?
-    document_name = Column(String(32))
-    document_description = Column(String(32))
-    document_full_description = Column(String)
-    document_filename = Column(String(253), nullable=False)  # Limit of current file systems
-    # Auto calculate extension
-    document_ext = Column(String(253), nullable=False)  # Total limit of filename and extension is 255
-    # document_path = Column(String)  # should belongs to same path as described in item
-
-    # relationships with other tables
-    user_id = Column(Integer, ForeignKey("user_accounts.user_id"))
-    user = relationship("PdmUser", back_populates="documents")
-    item_id = Column(Integer, ForeignKey("items.item_id"))
-    item = relationship("PdmItem", back_populates="models")
-
-    def __repr__(self):
-        # Fstrings are better ?
-        # https://realpython.com/python-f-strings/
-        return(f"PdmDocument(document_id={self.document_id!r}, document_number={self.document_number!r}, document_name={self.document_name!r}, document_description={self.document_description!r}, document_full_description={self.document_full_description!r}, document_filename={self.document_filename!r}, document_ext={self.document_ext!r})")
+        return(f"PdmManufacturer(manufacturer_id={self.manufacturer_id!r}, manufacturer_name={self.manufacturer_name!r})")
 
 
 class PdmMaterial(Base):
@@ -196,29 +145,53 @@ class PdmMaterial(Base):
         return(f"PdmMaterial(material_id={self.material_id!r}, material_name={self.material_name!r}, material_finish={self.material_finsh!r}, material_mass={self.material_mass!r}, material_mass_unit={self.material_mass_unit!r}, material_volume={self.material_volume!r}, material_volume_unit={self.material_volume_unit!r}, material_weight={self.material_weight!r}, material_weight_unit={self.material_weight_unit!r}, material_surface_area={self.material_surface_area!r}, material_surface_area_unit={self.material_surface_area_unit})")
 
 
-# TODO: A histric item / Model / Document can  have a foreign key to all of those tables.
-class PdmHistory(Base):
-    """Class with SQL History properties"""
-    __tablename__ = 'history'
+class PdmModel(Base):
+    """Class with SQL Item properties"""
+    __tablename__ = 'models'
 
-    history_id = Column(Integer, primary_key=True)
-    history_date_created = Column(Date)
-    history_created_by = Column(String)
-    history_date_last_edit = Column(Date)
-    history_last_edit_by = Column(String)
-    history_checked_out_by = Column(String)
-    # TODO: Create Complex revisions (Example: Date, major.minor, major.letter_minor)
-    history_revision_state = Column(Enum(pdm_enum.RevisionState))
-    history_revision_number = Column(Integer)  # Maybe other format
-    history_stored_number = Column(Integer)  # last store version iterator
-    # TODO: Every stored version in the database should be traceble
+    model_id = Column(Integer, primary_key=True)
+    model_number = Column(Integer)  # or should this be a string?
+    model_name = Column(String(32))
+    model_description = Column(String(32))
+    model_full_description = Column(String)
+    model_filename = Column(String(253), nullable=False)  # Limit of current file systems
+    # Auto calculate extension
+    model_ext = Column(String(253), nullable=False)  # Total limit of filename and extension is 255
+    # model_path = Column(String)  # should belongs to same path as described in item
+    model_preview = Column(LargeBinary)  # Change when no image is available
 
     # relationships with other tables
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    user = relationship("PdmUser", back_populates="models")
+    item_id = Column(Integer, ForeignKey("items.item_id"))
+    item = relationship("PdmItem", back_populates="models")
+    material = relationship("PdmMaterial", back_populates="model", uselist=False)
 
     def __repr__(self):
         # Fstrings are better ?
         # https://realpython.com/python-f-strings/
-        return(f"PdmHistory(history_id={self.history_id!r}, history_date_created={self.history_date_created!r}, history_created_by={self.history_created_by!r}, history_date_last_edit={self.history_date_last_edit!r}, history_last_edit_by={self.history_last_edit_by!r}, history_checked_out_by={self.history_checked_out_by!r}, history_revision_state={self.history_revision_state!r}, history_revision_number={self.history_revision_number!r}, history_stored_number={self.history_stored_number!r})")
+        return(f"PdmModel(model_id={self.model_id!r}, model_number={self.model_number!r}, model_name={self.model_name!r}, model_description={self.model_description!r}, model_full_description={self.model_full_description!r}, model_filename={self.model_filename!r}, model_ext={self.model_ext!r}, model_preview={self.model_preview!r})")
+
+
+class PdmProject(Base):
+    """Class with default SQL Role properties"""
+    __tablename__ = 'projects'
+
+    project_id = Column(Integer, primary_key=True)
+    project_number = Column(String(16), nullable=False)  # this can come another source as the Db so not same as project_id
+    project_name = Column(String(32))
+    project_status = Column(Enum(pdm_enum.ProjectState))
+    Project_date_start = Column(Date)
+    Project_date_finish = Column(Date)  # TODO check finish date is after start date
+    project_path = Column(String)
+
+    # relationships with other tables
+    users = relationship("PdmUsers", secondary="user_project_link", back_populates="projects")
+
+    def __repr__(self):
+        # Fstrings are better ?
+        # https://realpython.com/python-f-strings/
+        return(f"PdmProject(project_id={self.project_id!r}, project_number={self.project_number!r}, project_name={self.project_name!r}, project_status={self.project_status!r}, project_date_start={self.project_date_start!r}, project_date_finish={self.project_date_finish!r}, project_path={self.project_path!r})")
 
 
 class PdmPurchase(Base):
@@ -245,22 +218,49 @@ class PdmPurchase(Base):
         return(f"PdmPurchase(purchasing_id={self.purchasing_id!r}, purchasing_source={self.purchasing_source!r}, purchasing_tracebility={self.purchasing_tracebility!r})")
 
 
-class PdmManufacturer(Base):
-    """Class with SQL Manufacturing properties"""
-    __tablename__ = 'manufacturers'
+class PdmRole(Base):
+    """Class with default SQL Role properties"""
+    __tablename__ = 'roles'
 
-    manufacturer_id = Column(Integer, primary_key=True)
-    manufacturer_name = Column(String(32))
-    # TODO: Add manufacturer address
+    role_id = Column(Integer, primary_key=True)
+    role_name = Column(String(32))
+    # TODO: add privileges - Also how to
 
     # relationships with other tables
-    purchasing_id = Column(Integer, ForeignKey("purchasing.purchasing_id"))
-    purchasing = relationship("PdmPurchase", back_populates="manufacturers")
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    users = relationship("PdmUser", secondary="user_role_link", back_populates="roles")
 
     def __repr__(self):
         # Fstrings are better ?
         # https://realpython.com/python-f-strings/
-        return(f"PdmManufacturer(manufacturer_id={self.manufacturer_id!r}, manufacturer_name={self.manufacturer_name!r})")
+        return(f"PdmRole(role_id={self.role_id!r}, role_name={self.role_name!r})")
+
+
+class PdmUser(Base):
+    """Class with default SQL User properties"""
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String(30))
+    user_first_name = Column(String(30))
+    user_last_name = Column(String(30))
+    user_full_name = Column(String)
+    user_email_adress = Column(String, nullable=False)  # TODO: change to mail address
+    user_phonenumber = Column(Integer)  # TODO: add phonenumber property
+    user_department = Column(String)  # Enum optionally
+    # user_aliases = []  # TODO: What to do with aliases
+
+    # relationships with other tables
+    roles = relationship("PdmRole", secondary="user_role_link", back_populates="users")
+    projects = relationship("PdmProjects", secondary="user_project_link", back_populates="users")
+    items = relationship("PdmItem", back_populates="user")
+    models = relationship("PdmModel", back_populates="user")
+    documents = relationship("PdmDocument", back_populates="user")
+
+    def __repr__(self):
+        # Fstrings are better ?
+        # https://realpython.com/python-f-strings/
+        return f"PdmUser(user_id={self.user_id!r}, user_name={self.user_name!r}, user_first_name={self.user_first_name!r}, user_last_name={self.user_last_name!r}, user_full_name={self.user_full_name!r}, user_email_adress={self.user_email_adress!r}, user_phonenumber={self.user_phonenumber!r}, user_department={self.user_department!r})"
 
 
 class PdmVendor(Base):
@@ -284,18 +284,17 @@ class PdmVendor(Base):
 # relationships with other tables
 #
 
-class PdmUserRoleLink(Base):
-    """Association Table between user and project"""
-    # https://www.pythoncentral.io/sqlalchemy-association-tables/
-    __tablename__ = "user_role_link"
+class PdmProjectItemLink(Base):
+    """Association Table between Project and Item"""
+    __tablename__ = "Project_item_link"
 
-    user_id = Column("user_id", ForeignKey("users.user_id"), primary_key=True)
-    role_id = Column("role_id", ForeignKey("roles.role_id"), primary_key=True)
+    project_id = Column("project_id", ForeignKey("projects.project_id"), primary_key=True)
+    item_id = Column("item_id", ForeignKey("items.item_id"), primary_key=True)
 
     def __repr__(self):
         # Fstrings are better ?
         # https://realpython.com/python-f-strings/
-        return(f"Pdm User Role Association Table(user_id={self.user_id!r}, role_id={self.role_id!r})")
+        return(f"Pdm Project Item Association Table(project_id={self.project_id!r}, item_id={self.item_id!r})")
 
 
 class PdmUserProjectLink(Base):
@@ -312,14 +311,15 @@ class PdmUserProjectLink(Base):
         return(f"Pdm User Project Association Table(user_id={self.user_id!r}, project_id={self.project_id!r})")
 
 
-class PdmProjectItemLink(Base):
-    """Association Table between Project and Item"""
-    __tablename__ = "Project_item_link"
+class PdmUserRoleLink(Base):
+    """Association Table between user and project"""
+    # https://www.pythoncentral.io/sqlalchemy-association-tables/
+    __tablename__ = "user_role_link"
 
-    project_id = Column("project_id", ForeignKey("projects.project_id"), primary_key=True)
-    item_id = Column("item_id", ForeignKey("items.item_id"), primary_key=True)
+    user_id = Column("user_id", ForeignKey("users.user_id"), primary_key=True)
+    role_id = Column("role_id", ForeignKey("roles.role_id"), primary_key=True)
 
     def __repr__(self):
         # Fstrings are better ?
         # https://realpython.com/python-f-strings/
-        return(f"Pdm Project Item Association Table(project_id={self.project_id!r}, item_id={self.item_id!r})")
+        return(f"Pdm User Role Association Table(user_id={self.user_id!r}, role_id={self.role_id!r})")
