1c38ef7ea889986a92fd4b0fc70de2ad18d0a7db
Jee-Bee <winkeltj@e.email>
2022-12-17 18:42:11 +0100

    remove * from import. and remove u before strings, py3 default unicode.

--- src/gui/filter.py
+++ src/gui/filter.py
@@ -2,8 +2,8 @@ import sys
 import os
 from pathlib import Path
 
-from PySide2.QtCore import *  # type: ignore
-from PySide2.QtGui import *  # type: ignore
+from PySide2 import QtCore  # type: ignore
+from PySide2 import QtGui  # type: ignore
 import PySide2.QtWidgets as qtw  # type: ignore
 from PySide2.QtWidgets import QDialog  # type: ignore
 
@@ -17,41 +17,41 @@ class FilterDialog(QDialog):
 
         self.resize(400, 300)
         self.buttonBox = qtw.QDialogButtonBox(self)
-        self.buttonBox.setObjectName(u"buttonBox")
-        self.buttonBox.setGeometry(QRect(30, 240, 341, 32))
-        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setObjectName("buttonBox")  # Python3 default unicode
+        self.buttonBox.setGeometry(QtCore.QRect(30, 240, 341, 32))
+        self.buttonBox.setOrientation(QtGui.Qt.Horizontal)
         self.buttonBox.setStandardButtons(qtw.QDialogButtonBox.Cancel|qtw.QDialogButtonBox.Ok)
 
         self.show_fc_files_only = qtw.QCheckBox(self)
-        self.show_fc_files_only.setObjectName(u"show_fc_files_only")
-        self.show_fc_files_only.setGeometry(QRect(80, 20, 250, 17))
+        self.show_fc_files_only.setObjectName("show_fc_files_only")  # Python3 default unicode
+        self.show_fc_files_only.setGeometry(QtCore.QRect(80, 20, 250, 17))
 
         self.hide_versioned_fc_files = qtw.QCheckBox(self)
-        self.hide_versioned_fc_files.setObjectName(u"hide_versioned_fc_files")
-        self.hide_versioned_fc_files.setGeometry(QRect(80, 50, 250, 17))
+        self.hide_versioned_fc_files.setObjectName("hide_versioned_fc_files")  # Python3 default unicode
+        self.hide_versioned_fc_files.setGeometry(QtCore.QRect(80, 50, 250, 17))
 
         self.retranslate_ui()
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
 
-        QMetaObject.connectSlotsByName(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
 
         self.retrieve_data()
         self.show()
 
     def retranslate_ui(self):
-        self.setWindowTitle(QCoreApplication.translate("Filter Dialog", u"Filter Dialog", None))
-        self.show_fc_files_only.setText(QCoreApplication.translate("Dialog", u"Show FreeCAD files only", None))
-        self.hide_versioned_fc_files.setText(QCoreApplication.translate("Dialog", u"Hide versioned FreeCAD files", None))
+        self.setWindowTitle(QtCore.QCoreApplication.translate("Filter Dialog", "Filter Dialog", None))  # Python3 default unicode
+        self.show_fc_files_only.setText(QtCore.QCoreApplication.translate("Dialog", "Show FreeCAD files only", None))  # Python3 default unicode
+        self.hide_versioned_fc_files.setText(QtCore.QCoreApplication.translate("Dialog", "Hide versioned FreeCAD files", None))  # Python3 default unicode
 
     def retrieve_data(self):
         self.conf = config.conf()
         self.conf.read()
 
-        state = Qt.Checked if self.conf.get_filter(config.show_fc_files_only) else Qt.Unchecked
+        state = Qt.Checked if self.conf.get_filter(config.show_fc_files_only) else QtGui.Qt.Unchecked
         self.show_fc_files_only.setCheckState(state)
 
-        state = Qt.Checked if self.conf.get_filter(config.hide_versioned_fc_files) else Qt.Unchecked
+        state = Qt.Checked if self.conf.get_filter(config.hide_versioned_fc_files) else QtGui.Qt.Unchecked
         self.hide_versioned_fc_files.setCheckState(state)
 
     def store_data(self):
@@ -65,5 +65,5 @@ class FilterDialog(QDialog):
 
 def filter_dialog():
     filter_dlg = FilterDialog()
-    if filter_dlg.exec_() == 1: # Ok button pushed
+    if filter_dlg.exec_() == 1:  # Ok button pushed
         filter_dlg.store_data()
