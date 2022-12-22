eb6b37eb6316eaa39cc1a1fd06a73f65ccb23fc9
Jee-Bee <winkeltj@e.email>
2022-12-22 19:59:37 +0100

    Update loading and empty / base  functions

--- src/gui/authenticate.py
+++ src/gui/authenticate.py
@@ -9,6 +9,7 @@ import sys
 import os
 from pathlib import Path
 
+from PySide2 import QtCore  # type: ignore
 from PySide2.QtWidgets import QDialog  # type: ignore
 from PySide2.QtCore import QFile  # type: ignore
 from PySide2.QtUiTools import QUiLoader  # type: ignore
@@ -21,11 +22,6 @@ class Authenticate(QDialog):
     def __init__(self):
         super(Authenticate, self).__init__()
 
-        self.load_ui()
-        # self.buttonBox.accepted.connect(self.accept)
-        # self.buttonBox.rejected.connect(self.reject)
-
-    def load_ui(self):
         loader = QUiLoader()
         path = os.fspath(Path(__file__).resolve().parents[1] / "gui/Authenticate.ui")
         print(path)
@@ -33,17 +29,53 @@ class Authenticate(QDialog):
         ui_file.open(QFile.ReadOnly)
         self.ui = loader.load(ui_file, self)
         self.ui.setGeometry(400, 300, 400, 300)
-        self.ui.setWindowTitle("Login to Db")
-        print("before show")
-        self.ui.show()
-        print("after show")
+        self.ui.setWindowTitle("Database Authentication")
+        # self.ui.setModal(False)
+        # self.ui.setWindowModality(QtCore.Qt.WindowModality(0))
+        self.ui.buttonApply.setEnabled(False)
+
+        # self.buttonBox.accepted.connect(self.accept)
+        # self.buttonBox.rejected.connect(self.reject)
+
+        # button clicked
+        self.ui.buttonCancel.clicked.connect(self.cancel_auth)
+        self.ui.buttonConnect.clicked.connect(self.connect_auth)
+        self.ui.buttonApply.clicked.connect(self.apply_auth)
+
+        # somehow show don't work out
+        # self.ui.show()
+        self.ui.exec()
+
+    def load_ui(self):
+        """Load default authentication dialog"""
+        pass
+
+    def cancel_auth(self):
+        """Cancel authentication"""
+        # Bring back in previous state
+        print("in cancel_auth")
+        self.ui.close()
+
+    def connect_auth(self):
+        """Connect authentication"""
+        # TODO: Connect to server
+        # If connect can be applied than
+        # - set properety
+        # - activater Apply
+        # Else
+        # - Show labReturnMessage - Connection failed
+        pass
+
+    def apply_auth(self):
+        """Apply authentication"""
+        # TODO: Apply connection == save connection state
+        pass
 
 
 def authenticate_dialog():
     """Start Login / Authentication dialog"""
     print("dialog is running")
     auth = Authenticate()
-    auth.show()
 
 
 if __name__ == "__main__":
